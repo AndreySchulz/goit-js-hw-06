1b4c3b17f6642093901913d6d7557f2e6ce65fe2
Andrey_Schulz <andreygorb040693@gmail.com>
2022-09-21 18:18:56 +0300

    complite 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyRef: document.querySelector('body'),
+  textRef: document.querySelector('.color'),
+  btnRef: document.querySelector('.change-color')
+}
+function getNewColor(){
+  refs.bodyRef.style.backgroundColor = getRandomHexColor();
+  refs.textRef.textContent = `${getRandomHexColor()}`
+}
+refs.btnRef.addEventListener("click" , getNewColor)
