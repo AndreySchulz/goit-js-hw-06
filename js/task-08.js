33adf4602de67944fe27ded1d8f0fe44869c44aa
Andrey_Schulz <andreygorb040693@gmail.com>
2022-09-21 16:57:48 +0300

    complite 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
-
+const loginFormRef = document.querySelector('.login-form');
+let formInfo = []
+loginFormRef.addEventListener("submit", (event)=>{
+    event.preventDefault();
+    const {elements: {email, password}}= event.currentTarget;
+    if(email.value=== "" || password.value === ""){
+        alert("все поля должны быть заполнены.")
+    }
+    formInfo.email = email.value
+    formInfo.password = password.value
+    formInfo.reset
+    console.log(formInfo)
+})
