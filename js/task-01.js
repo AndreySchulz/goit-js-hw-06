3293af8e5b3825a52dbef469ef0062c9e0a83047
Andrey_Schulz <andreygorb040693@gmail.com>
2022-09-23 11:05:19 +0300

    Update task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
 const categoriesListRef = document.querySelector("#categories");
 const categoriesRef = document.querySelectorAll('.item');
-
+console.log(`Number of categories:${categoriesListRef.children.length}`)
 for(const element of categoriesRef){
-
+    
     console.log(`Category: ${element.querySelector("h2").textContent}`);
     console.log(`Elements: ${element.querySelectorAll("li").length}`);
 }
